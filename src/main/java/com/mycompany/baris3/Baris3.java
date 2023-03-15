72634f5ba8711fdd4d2502b315a4d46ecf6148c1
PurwaDesriyandi <111819638+PurwaDesriyandi@users.noreply.github.com>
2023-03-15 09:33:21 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/baris3/Baris3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.baris3;
+
+/**
+ *
+ * @author purwa
+ */
+public class Baris3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
